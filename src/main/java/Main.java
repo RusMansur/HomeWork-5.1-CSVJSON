fdd2855be22eeebf6535b8a0700950a5707c2db7
Руслан Долгополов <rusdolg@gmail.com>
2022-03-09 14:44:11 +0300

    Первоначальная версия

---
+++ src/main/java/Main.java
@@ -0,0 +1,62 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import com.opencsv.CSVReader;
+import com.opencsv.bean.ColumnPositionMappingStrategy;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
+        String fileName = "data.csv";
+        List<Employee> list = parseCSV(columnMapping, fileName);
+        String json = listToJson(list);
+        writeString(json);
+    }
+
+    private static void writeString(String json) {
+        try (FileWriter fileWriter = new FileWriter(new File("data.json"))) {
+            fileWriter.write(json);
+            fileWriter.flush();
+        } catch (IOException ex) {
+            System.err.println("Не удалось записать файл");
+        }
+    }
+
+    private static String listToJson(List<Employee> list) {
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.create();
+        Type listType = new TypeToken<List<Employee>>() {
+        }.getType();
+        String json = gson.toJson(list, listType);
+        return json;
+    }
+
+    private static List<Employee> parseCSV(String[] columnMapping, String fileName) {
+        List<Employee> list = null;
+        try (CSVReader csvReader = new CSVReader(new FileReader(fileName))) {
+
+            ColumnPositionMappingStrategy<Employee> strategy = new ColumnPositionMappingStrategy<>();
+            strategy.setType(Employee.class);
+            strategy.setColumnMapping(columnMapping);
+
+            CsvToBean<Employee> csvToBean = new CsvToBeanBuilder<Employee>(csvReader)
+                    .withMappingStrategy(strategy)
+                    .build();
+
+            list = csvToBean.parse();
+        } catch (IOException ex) {
+            System.err.println("Не удалось прочитать файл");
+        }
+        return list;
+    }
+}
